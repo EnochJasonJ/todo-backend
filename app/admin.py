0cf3ecb203541962a25dc95efc0ff21cdb6100d1
Enoch Jason J <enochjasonj@Enochs-MacBook-Air.local>
2025-07-04 22:47:04 +0530

    Customized TaskModel in the admin site.

--- app/admin.py
+++ app/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 from .models import TaskModel
 
 # Register your models here.
-admin.site.register(TaskModel)
+class TaskModelAdmin(admin.ModelAdmin):
+    list_display = ('title','user','is_archived','created_at')
+    list_filter = ('user','is_archived')
+    search_fields = ('title','user__username')
+
+admin.site.register(TaskModel, TaskModelAdmin)
