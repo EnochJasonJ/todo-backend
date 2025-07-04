1a3b2154690bd3fa6fd1c2c521f7299796c50a2e
Enoch Jason J <enochjasonj@Enochs-MacBook-Air.local>
2025-07-04 15:05:50 +0530

    Changed Allowed hosts and added backend link to celery

--- app/tasks.py
+++ app/tasks.py
@@ -4,7 +4,7 @@ import requests as rq
 @shared_task
 def ping_self():
     try:
-        rq.get('')
+        rq.get('https://todo-backend-27zr.onrender.com/hello/')
         print("✅ pinged self to keep render awake")
     except Exception as err:
         print(f"❌ Ping failed: {err}")
