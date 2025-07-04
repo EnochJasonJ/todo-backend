3d0416602bf21488c2c67057ec8f4e004abae558
Enoch Jason J <enochjasonj@Enochs-MacBook-Air.local>
2025-07-04 20:42:41 +0530

    Added oAuth for google.

--- app/urls.py
+++ app/urls.py
@@ -1,17 +1,17 @@
-from django.urls import path
+from django.urls import path, include
 from .views import hello, Register, Login, ChangePassword, TaskView, TaskDetailView, ArchiveTask, ArchievedTaskView, UnArchiveTask
 from rest_framework.authtoken.views import obtain_auth_token
 urlpatterns = [
-    path("hello/", hello, name="hello"),
-    path('register/', Register.as_view(), name='register'),
-    path('login/', Login.as_view(), name='login'),
-    path('change-password/', ChangePassword.as_view(), name='change_password'),
+    path("hello/", hello),
+    path('register/', Register.as_view()),
+    path('login/', Login.as_view()),
+    path('change-password/', ChangePassword.as_view()),
     path('api-token-auth/', obtain_auth_token),
-    path('todo/',TaskView.as_view(),name="todo"),
-    path('todo/<int:pk>/',TaskDetailView.as_view()),
-    path('archive-task/<int:pk>/',ArchiveTask.as_view(),name="archive-task"),
-    path('archieved-task/',ArchievedTaskView.as_view(),name="archived-task"),
-    path('unarchive-task/<int:pk>/', UnArchiveTask.as_view(), name="unarchive-task"),
-
-
+    path('todo/', TaskView.as_view()),
+    path('todo/<int:pk>/', TaskDetailView.as_view()),
+    path('archive-task/<int:pk>/', ArchiveTask.as_view()),
+    path('archieved-task/', ArchievedTaskView.as_view()),
+    path('unarchive-task/<int:pk>/', UnArchiveTask.as_view()),
+    path('auth/', include('dj_rest_auth.urls')),
+    path('auth/registration/', include('dj_rest_auth.registration.urls')),
 ]
